9c9c587bfc33ff8ae80187cc65592e968de1364c
phananhduc2003 <pananhduc2003@gmail.com>
2025-05-19 11:58:10 +0700

    initialize project

---
+++ BE/tourease-api/src/main/java/com/tourease/api/TourEaseApplication.java
@@ -0,0 +1,13 @@
+package com.tourease.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourEaseApplication.class, args);
+	}
+
+}
