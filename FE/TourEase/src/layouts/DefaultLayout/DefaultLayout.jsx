ba1896f6f06cddfc8ac64c6300223b0672cb1a63
phananhduc2003 <pananhduc2003@gmail.com>
2025-11-11 12:03:06 +0700

    add_animation_for_transition_page_and_fix_ui

--- FE/TourEase/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ FE/TourEase/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
 
-import { Box, Typography } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const wrapper = {
   width: "100%",
@@ -19,11 +21,41 @@ const content = {
 };
 
 function DefaultLayout({ children }) {
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 600);
+
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
+
   return (
     <>
       <Box sx={wrapper}>
         <Header />
-        <Box sx={content}>{children}</Box>
+        <Box sx={content}>
+          {isLoading ? (
+            <Box
+              sx={{
+                position: "relative",
+                minHeight: "300px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CircularProgress />
+              <Typography sx={{ ml: 2 }}>Đang tải...</Typography>
+            </Box>
+          ) : (
+            children
+          )}
+        </Box>
         <Footer />
       </Box>
     </>
